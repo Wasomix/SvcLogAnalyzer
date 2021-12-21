31f63c8f6d96553e709c66af7ce43a92bbef8e7c
Sergio Ferrandiz Calero <sergio.ferrandiz-calero@capgemini.com>
2021-12-21 03:12:14 +0100

    Code refactor. Addition of new class for searching pattern.

---
+++ SvcLogAnalyzerBackEnd/SvcLogAnalyzerBackEnd/PatternSearcher.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+
+namespace SvcLogAnalyzerBackEnd
+{
+    /// <summary>
+    /// This class is responsible for searching the pattern
+    /// </summary>
+    public class PatternSearcher
+    {
+        private bool _patternFound;
+        private const int BUFFER_SIZE = 4096;
+
+        public PatternSearcher()
+        {
+            ResetPatternFound();
+        }
+
+        private void ResetPatternFound()
+        {
+            _patternFound = false;
+        }
+
+        public bool ItContainsPattern(StreamReader reader, string patternToSearch)
+        {            
+            char[] buffer = new char[BUFFER_SIZE];
+
+            ResetPatternFound();
+
+            while (((reader.Read(buffer, 0, buffer.Length)) > 0) && (IsPatternNotFound()))
+            {
+                if (ItContainsPattern(buffer, patternToSearch))
+                {
+                    _patternFound = true;                    
+                }
+            }
+
+            return IsPatternFound();
+        }
+
+        private bool ItContainsPattern(char[] buffer, string patternToSearch)
+        {
+            var bufferRead = string.Join("", buffer);
+            return bufferRead.Contains(patternToSearch);
+        }
+
+        private bool IsPatternNotFound()
+        {
+            return !IsPatternFound();
+        }
+
+        private bool IsPatternFound()
+        {
+            return _patternFound;
+        }
+    }
+}
--- SvcLogAnalyzerBackEnd/SvcLogAnalyzerBackEnd/SvcLogFilesSearcher.cs
+++ SvcLogAnalyzerBackEnd/SvcLogAnalyzerBackEnd/SvcLogFilesSearcher.cs
@@ -7,71 +7,37 @@ namespace SvcLogAnalyzerBackEnd
 {
     public class SvcLogFilesSearcher
     {
-        private string _patterToSearch = "200006199089";
+        private string _patternToSearch;
+        private List<string> _filesContainingPattern;
+        List<string> _svcFileNames;
+
+        public SvcLogFilesSearcher()
+        {
+            _patternToSearch = "200006199089";
+            _filesContainingPattern = new List<string>();
+            _svcFileNames = new List<string>();
+        }
+
         public void Run()
         {
             string prefixName = "TransferWorker_Messages__";
             string suffixName = "transferworker_in_0.svclog";
             const int NUMBER_OF_FILES = 53;
-            const int INITIAL_VALUE = 1;
-            
-            string filePath = @"C:\Users\sferrand\Sergio\00_ACTUAL\Capgemini\Projects\PostNL\IncidentsWorkitem\Active\INCIDENT 26789\20211213";
-
-            List<string> filesContainingPattern = new List<string>();
-            List<string> fileNames = new List<string>();
+            const int INITIAL_VALUE = 1;            
+            string filePath = @"C:\Users\sferrand\Sergio\00_ACTUAL\Capgemini\Projects\PostNL\IncidentsWorkitem\Active\INCIDENT 26789\20211213";           
 
             try
             {
                 for (int i = INITIAL_VALUE; i < NUMBER_OF_FILES; i++)
                 {
                     string fileNameToAdd = prefixName + i.ToString() + suffixName;
-                    fileNames.Add(fileNameToAdd);
+                    _svcFileNames.Add(fileNameToAdd);
                 }
 
-                foreach (string fileName in fileNames)
-                {
-                    string fileNamePath = filePath + @"\" + fileName;
-
-                    if (File.Exists(fileNamePath))
-                    {
-                        System.Console.WriteLine($"Start processing file {fileName}");
-
-                        File.Copy(fileNamePath, fileName, true);
-
-                        var filestream = new FileStream(fileNamePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete, 4096, FileOptions.RandomAccess);
-                        StreamReader reader = new StreamReader(filestream, Encoding.ASCII);
-
-                        long fromPosition = 0;
-                        reader.BaseStream.Seek(fromPosition, SeekOrigin.Begin);
-                        reader.DiscardBufferedData();
-                        char[] buffer = new char[4096];
-                        int charsRead;
-                        //int count = 0;
-                        //long result = -1;
-                        bool patternNotFound = true;
-                        while (((charsRead = reader.Read(buffer, 0, buffer.Length)) > 0) && (patternNotFound))
-                        {
-                            if(ItContainsPattern(buffer))
-                            {
-                                patternNotFound = false;
-                                filesContainingPattern.Add(fileName);
-                            }
-                        }
-
-                        File.Delete(fileName);
-                        System.Console.WriteLine($"End processing file {fileName}");
-                        System.Console.WriteLine("");
-                    }
-                }
-
-                System.Console.WriteLine("");
-                System.Console.WriteLine("");
-                System.Console.WriteLine("");
-
-                foreach (var fileName in filesContainingPattern)
-                {
-                    System.Console.WriteLine(fileName);
-                }
+                SearchPatternInSvcLogFiles(filePath);
+                PrintNumberOfNewLines(3);
+                PrintFileNamesContainingPattern();
+                
             }
             catch (Exception ex)
             {
@@ -79,10 +45,62 @@ namespace SvcLogAnalyzerBackEnd
             }
         }
 
-        private bool ItContainsPattern(char[] buffer)
+        private void SearchPatternInSvcLogFiles(string filePath)
+        {            
+            foreach (string fileName in _svcFileNames)
+            {
+                string fileNamePath = filePath + @"\" + fileName;
+
+                if (File.Exists(fileNamePath))
+                {
+                    ProcessSvcLogFile(fileNamePath, fileName);
+                }
+            }
+        }
+
+        private void ProcessSvcLogFile(string fileNamePath, string fileName)
         {
-            var bufferRead = string.Join("", buffer);
-            return bufferRead.Contains(_patterToSearch);
+            PatternSearcher patternSearcher = new PatternSearcher();
+
+            System.Console.WriteLine($"Start processing file {fileName}");
+
+            File.Copy(fileNamePath, fileName, true);
+            StreamReader reader = SetUpStreamReader(fileNamePath);
+
+            if (patternSearcher.ItContainsPattern(reader, _patternToSearch))
+            {
+                _filesContainingPattern.Add(fileName);
+            }
+
+            File.Delete(fileName);
+            System.Console.WriteLine($"End processing file {fileName}");
+            PrintNumberOfNewLines(1);
+        }
+
+        private StreamReader SetUpStreamReader(string fileNamePath)
+        {
+            var filestream = new FileStream(fileNamePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete, 4096, FileOptions.RandomAccess);
+            StreamReader reader = new StreamReader(filestream, Encoding.ASCII);
+            long fromPosition = 0;
+            reader.BaseStream.Seek(fromPosition, SeekOrigin.Begin);
+            reader.DiscardBufferedData();
+            return reader;
+        }
+
+        private void PrintFileNamesContainingPattern()
+        {
+            foreach (var fileName in _filesContainingPattern)
+            {
+                System.Console.WriteLine(fileName);
+            }
+        }
+
+        private void PrintNumberOfNewLines(int numberOfNewLines)
+        {
+            for(int i=0; i<numberOfNewLines; i++)
+            {
+                System.Console.WriteLine("");
+            }            
         }
     }
 }
