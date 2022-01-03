6d092cdaefe79703425170cb43e0dfcec2f2c3e9
Sergio Ferrandiz Calero <sergio.ferrandiz-calero@capgemini.com>
2022-01-03 08:55:56 +0100

    Addition of unit test, but they fail due to stream reader. It must be refactored.

--- SvcLogAnalyzerBackEnd/SvcLogAnalyzerBackEndTest/PatternSearcherTest.cs
+++ SvcLogAnalyzerBackEnd/SvcLogAnalyzerBackEndTest/PatternSearcherTest.cs
@@ -1,14 +1,59 @@
-﻿using Xunit;
+﻿using SvcLogAnalyzerBackEnd;
+using System;
+using System.IO;
+using Xunit;
 
 namespace SvcLogAnalyzerBackEndTest
 {
     public class PatternSearcherTest
     {
+        private PatternSearcher _patternSearcher;
+        private StreamReader _streamReader;
+        private string _fileNamePath;
+        private string _fileContent;
+        private string _pattern;
+        public PatternSearcherTest()
+        {
+            _fileNamePath = "TestFile.svclog";
+            _pattern = "";
+            _patternSearcher = new PatternSearcher();
+            CreateFile();
+            _streamReader = new StreamReader(_fileNamePath);
+        }
+        
+        private void CreateFile()
+        {
+            _fileContent = "New text for testing\na method with pattern = 12rt\nin unit testing";
+            File.WriteAllText(_fileNamePath, _fileContent);
+        }
+        
+        private void TearDown()
+        {
+            DeleteFileIfItExist(_fileNamePath);
+        }
+
+        private void DeleteFileIfItExist(string fileNamePath)
+        {
+            FileWrapper.DeleteFileIfItExist(_fileNamePath);
+        }
+
         // TODO : Make all test cases with correct naming convention
         [Fact]
-        public void ItContainsPattern_FindSvclogFiles_ReturnRightAmountOfFiles()
-        {
+        public void ItContainsPattern_FindPattern_ReturnTrue()
+        {            
+            _pattern = "12rt";
+            bool patternFound = _patternSearcher.ItContainsPattern(_streamReader, _pattern);
+            TearDown();
+            Assert.True(patternFound);
+        }
 
+        [Fact]
+        public void ItContainsPattern_DoNotFindPattern_ReturnFalse()
+        {
+            _pattern = "sA34";
+            bool patternFound = _patternSearcher.ItContainsPattern(_streamReader, _pattern);
+            TearDown();
+            Assert.False(patternFound);
         }
     }
 }
